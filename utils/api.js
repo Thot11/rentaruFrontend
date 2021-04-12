773c9d22857ea6645c406b2e26d141055d4f25de
Florian Durand <contact@floriandurand.com>
2021-04-12 16:01:43 +0200

    clean api file

--- utils/api.js
+++ utils/api.js
@@ -1,73 +1,13 @@
 import axios from 'axios'
 
+// Helpers
+
 export function getStrapiURL(path) {
   return `${
     process.env.NEXT_PUBLIC_STRAPI_API_URL
   }${path}`;
 }
 
-  export async function postConnect(mail, password) {
-
-    const { data } = await axios
-    .post(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/auth/local`, {
-      identifier: mail,
-      password: password,
-    })
-
-    return data
-  }
-
-  export async function postSignUp(
-    mail,
-    password,
-    ville,
-    departement,
-    prenom,
-    nom,
-    phone,
-    username,
-    address,
-    selectedFile
-    ) {
-
-    const {data} = await axios
-    .post(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/auth/local/register`, {
-        email: mail,
-        password,
-        username,
-        confirmed: true,
-        ville,
-        departement,
-        prenom,
-        nom,
-        phone,
-        address,
-        profilPic : selectedFile
-    })
-
-    return data
-  }
-
-  export async function postTokenToUser(data) {
-
-    const resp = await axios
-    .post(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/tokens`, {
-      token: data.jwt,
-      user: data.user.id,
-    })
-    
-    return resp
-  }
-
-  export async function postUploadImg(data, token) {
-
-    const resp = await axios
-    .post(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/upload?token=${token}`,data)
-    
-    return resp
-  }
-
-// Helper to make GET requests to Strapi
 export async function fetchAPI(path) {
   const requestUrl = getStrapiURL(path);
   const response = await fetch(requestUrl);
@@ -75,30 +15,7 @@ export async function fetchAPI(path) {
   return data;
 }
 
-export async function getCategories() {
-  const categories = await fetchAPI("/categories");
-  return categories;
-}
-
-export async function getMe(token) {
-  const { data } = await axios
-  .get(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/users/me?token=${token}`)
-  
-  return data;
-}
-
-export async function updateMe(data, token) {
-  const resp = await axios
-  .put(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/users/me?token=${token}`, data)
-  
-  return resp;
-}
-
-export async function postProduct(data, token) {
-  const resp = await axios
-  .post(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/products?token=${token}`, data)
-  return resp;
-}
+// GET Request with strapi helper
 
 export async function getCategory(slug) {
   const categories = await fetchAPI(`/categories?slug=${slug}`);
@@ -128,3 +45,95 @@ export async function getProduct(slug) {
   const products = await fetchAPI(`/products?slug=${slug}`);
   return products?.[0];
 }
+
+export async function getCategories() {
+  const categories = await fetchAPI("/categories");
+  return categories;
+}
+
+export async function getMe(token) {
+  const me = await fetchAPI(`/users/me?token=${token}`);
+  return me;
+}
+
+// POST Request
+
+export async function postConnect(mail, password) {
+
+  const { data } = await axios
+  .post(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/auth/local`, {
+    identifier: mail,
+    password: password,
+  })
+
+  return data
+}
+
+export async function postSignUp(
+  mail,
+  password,
+  ville,
+  departement,
+  prenom,
+  nom,
+  phone,
+  username,
+  address,
+  selectedFile
+  ) {
+
+  const {data} = await axios
+  .post(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/auth/local/register`, {
+      email: mail,
+      password,
+      username,
+      confirmed: true,
+      ville,
+      departement,
+      prenom,
+      nom,
+      phone,
+      address,
+      profilPic : selectedFile
+  })
+
+  return data
+}
+
+export async function postTokenToUser(data) {
+
+  const resp = await axios
+  .post(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/tokens`, {
+    token: data.jwt,
+    user: data.user.id,
+  })
+  
+  return resp
+}
+
+export async function postUploadImg(data, token) {
+
+  const resp = await axios
+  .post(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/upload?token=${token}`,data)
+  
+  return resp
+}
+
+
+
+export async function postProduct(data, token) {
+  const resp = await axios
+  .post(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/products?token=${token}`, data)
+  return resp;
+}
+
+// PUT Request
+
+export async function updateMe(data, token) {
+  const resp = await axios
+  .put(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/users/me?token=${token}`, data)
+  
+  return resp;
+}
+
+
