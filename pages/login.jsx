3d90bdd7f032567cd304d024e95b9d51932be7c9
Mortikus <mott@scorf.io>
2021-04-12 16:07:47 +0200

    color globalize

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,45 +1,66 @@
+/* eslint-disable prettier/prettier */
 import Head from "next/head";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useRouter } from 'next/router'
-import { postConnect, logOut } from '../store'
+import { useRouter } from "next/router";
+import { postConnect, logOut } from "../store";
 
 const Login = () => {
-
   const dispatch = useDispatch();
-  const router = useRouter()
+  const router = useRouter();
   const { session } = useSelector((state) => state);
 
-  const [mail, setMail] = useState('');
-  const [password, setPassword] = useState('');
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
-    if (session) router.push('/')
-  }, [session])
-  
+    if (session) router.push("/");
+  }, [session]);
+
   const connect = (mail, password) => {
-    dispatch(postConnect(mail, password))
-  }
+    dispatch(postConnect(mail, password));
+  };
 
   const disconnect = () => {
-    dispatch(logOut())
-  }
+    dispatch(logOut());
+  };
 
   return (
     <div>
       <Head>
         <title>Login</title>
       </Head>
-      <input type="text" defaultValue={mail} onChange={(e) => setMail(e.target.value)} />
-      <input type="password" defaultValue={password} onChange={(e) => setPassword(e.target.value)} />
-      <div onClick={() => connect(mail, password)}>Submit</div>
-      <div onClick={disconnect}>Log Out</div>
-      <Link href={'/signUp'}> Inscription </Link>
+      <div className="loginContainer">
+        <img className="background" src="/japBckg.svg" alt="background"/>
+        <h2>Renseignez vos identifiants</h2>
+        <div className="loginWrapper">
+          <h3>Connexion</h3>
+          <input
+            type="text"
+            defaultValue={mail}
+            placeholder='E-mail'
+            onChange={(e) => setMail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder='Mot de passe'
+            defaultValue={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="rememberMe">
+            <p>Se souvenir de moi ?</p>
+            <input type="checkbox"/>
+          </div>
+          <button className="btnUnderline">Mot de passe oublié ?</button>
+          <div className="buttons">
+            <button onClick={() => connect(mail, password)}>Connexion</button>
+            <button><Link href={"/signUp"}> Inscription </Link></button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
-
-
 
 export default Login;
--- pages/signUp.jsx
+++ pages/signUp.jsx
@@ -1,48 +1,50 @@
+/* eslint-disable prettier/prettier */
 import Head from "next/head";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useRouter } from 'next/router'
-import { postSignUp } from '../store'
+import { useRouter } from "next/router";
+import { postSignUp } from "../store";
 
 const SignUp = () => {
-
   const dispatch = useDispatch();
-  const router = useRouter()
+  const router = useRouter();
   const { session } = useSelector((state) => state);
 
-  const [mail, setMail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [ville, setVille] = useState('');
-  const [departement, setDepartement] = useState('');
-  const [prenom, setPrenom] = useState('');
-  const [nom, setNom] = useState('');
-  const [phone, setPhone] = useState('');
-  const [username, setUsername] = useState('');
-  const [address, setAddress] = useState('');
-  const [selectedFile, setSelectedFile] = useState()
-  
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [ville, setVille] = useState("");
+  const [departement, setDepartement] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [nom, setNom] = useState("");
+  const [phone, setPhone] = useState("");
+  const [username, setUsername] = useState("");
+  const [address, setAddress] = useState("");
+  const [selectedFile, setSelectedFile] = useState();
+
   const signUp = () => {
     if (password === confirmPassword) {
-      dispatch(postSignUp(
-        mail,
-        password,
-        ville,
-        departement,
-        prenom,
-        nom,
-        phone,
-        username,
-        address,
-        selectedFile
-        ))
+      dispatch(
+        postSignUp(
+          mail,
+          password,
+          ville,
+          departement,
+          prenom,
+          nom,
+          phone,
+          username,
+          address,
+          selectedFile
+        )
+      );
     }
-  }
+  };
 
   useEffect(() => {
-    if (session) router.push('/')
-  }, [session])
+    if (session) router.push("/");
+  }, [session, router]);
 
   return (
     <div>
@@ -51,37 +53,75 @@ const SignUp = () => {
       </Head>
       <div className="signInContainer">
         mail
-        <input type="email" defaultValue={mail} onChange={(e) => setMail(e.target.value)} />
+        <input
+          type="email"
+          defaultValue={mail}
+          onChange={(e) => setMail(e.target.value)}
+        />
         username
-        <input type="text" defaultValue={username} onChange={(e) => setUsername(e.target.value)} />
+        <input
+          type="text"
+          defaultValue={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
         prenom
-        <input type="text" defaultValue={prenom} onChange={(e) => setPrenom(e.target.value)} />
+        <input
+          type="text"
+          defaultValue={prenom}
+          onChange={(e) => setPrenom(e.target.value)}
+        />
         nom
-        <input type="text" defaultValue={nom} onChange={(e) => setNom(e.target.value)} />
+        <input
+          type="text"
+          defaultValue={nom}
+          onChange={(e) => setNom(e.target.value)}
+        />
         adresse
-        <input type="text" defaultValue={address} onChange={(e) => setAddress(e.target.value)} />
+        <input
+          type="text"
+          defaultValue={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
         ville
-        <input type="text" defaultValue={ville} onChange={(e) => setVille(e.target.value)} />
+        <input
+          type="text"
+          defaultValue={ville}
+          onChange={(e) => setVille(e.target.value)}
+        />
         departement
-        <input type="number" defaultValue={departement} onChange={(e) => setDepartement(e.target.value)} />
+        <input
+          type="number"
+          defaultValue={departement}
+          onChange={(e) => setDepartement(e.target.value)}
+        />
         telephone
-        <input type="tel" defaultValue={phone} onChange={(e) => setPhone(e.target.value)} />
+        <input
+          type="tel"
+          defaultValue={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
         mdp
-        <input type="password" defaultValue={password} onChange={(e) => setPassword(e.target.value)} />
+        <input
+          type="password"
+          defaultValue={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         confirm mdp
-        <input type="password" defaultValue={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+        <input
+          type="password"
+          defaultValue={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
         Photo de profil
         <input
           type="file"
           onChange={(e) => setSelectedFile(e.target.files[0])}
         />
         <div onClick={signUp}>Submit</div>
-        <Link href={'/login'}> Connexion </Link>
+        <Link href={"/login"}> Connexion </Link>
       </div>
     </div>
   );
 };
-
-
 
 export default SignUp;
