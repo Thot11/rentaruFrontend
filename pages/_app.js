e9c6ca9e6973c7d718c230edd713bb7215af729b
Thot11 <marc.ott@hetic.net>
2021-06-16 09:54:00 +0200

    gtm

--- pages/_app.js
+++ pages/_app.js
@@ -30,9 +30,10 @@ const MyApp = ({ Component, pageProps }) => {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NH72Q3F');`}} ></script>
-          <noscript dangerouslySetInnerHTML={{__html:`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NH72Q3F"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+          })(window,document,'script','dataLayer','GTM-PC3BPBZ');`}} ></script>
+
+          <noscript dangerouslySetInnerHTML={{__html:`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PC3BPBZ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
         </Head>
         <Component {...pageProps} />
       </Layout>
