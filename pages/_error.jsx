006e49da9443f54f5470e7342536f6b76a06d1de
Florian Durand <contact@floriandurand.com>
2021-06-28 12:16:57 +0200

    some tests

---
+++ pages/_error.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import Button from "../elements/Button";
+import Link from "next/link";
+
+export default function Custom404() {
+  return (
+    <div className="div404">
+      <h1>404</h1>
+      <div className="sushi">Ooops, Il y a un sushi.</div>
+      <div className="notFound">page not found</div>
+      <img src="/sushi.svg" alt="sushi404" />
+      <Link href="/">
+        <a>
+          <Button color="Red">Go Home</Button>
+        </a>
+      </Link>
+    </div>
+  );
+}
